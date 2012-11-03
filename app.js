12f37c9ee2d1f90fb8492cb49abba0df952404a8
André Fiedler <kontakt@visualdrugs.net>
2012-11-03 13:46:21 -0400

    more work on parsing mjpeg

--- app.js
+++ app.js
@@ -1,103 +1,105 @@
-console.log('init');
+window.addEventListener('DOMContentLoaded', function() {
 
-var request = new XMLHttpRequest();
+    console.log('init');
+    
+    var start = 0;
 
-// The actual image
-var imageBytes = [];
+    // Open the stream
+    var request = new XMLHttpRequest();
+    request.open('GET', 'stream/stream_chunk_small', true);
+    request.responseType = 'arraybuffer';
+    request.onprogress = function updateProgress(event) {
+        if (event.lengthComputable) {
+            var percentComplete = Math.round((event.loaded / event.total) * 100);
+            console.log('Loading: ' + percentComplete + ' %');
+        }
+    };
+    request.onload = function(event) {
 
-// The chars at the end of the image
-var endPos = "\n--myboundary";
+        console.log('running');
 
-// Started to find, finished finding
-var done = false;
-var started = false;
+        var mjpegBuffer = request.response;
 
-// Don't know why I have to save these to a ByteArray to do the comparisons but it seems I do
-var startBytes = [];
-var startByte = 0xFF;
-var secondByte = 0xD8;
+        var image = [];
+        var end = 0;
+        var offset = start;
+        var marker = [];
+        var value = false;
 
-startBytes.push(0xFF);
-startBytes.push(0xD8);
+        // If there are bytes and the offset is at the start
+        // Look for a JPEG image
+        if (mjpegBuffer) {
+            var byteArray = new Uint8Array(mjpegBuffer);
 
+            if (start === 0) {
+                // Look for the start of the JPEG
+                for (offset; offset < byteArray.byteLength - 1; offset++) {
+                    marker[0] = byteArray[offset];
+                    marker[1] = byteArray[offset + 1];
 
-var startNum = startBytes[0];
-var nextNum = startBytes[1];
-
-// Open the stream
-var request = new XMLHttpRequest();
-request.open('GET', 'stream/stream_chunk', true);
-request.responseType = 'arraybuffer';
-request.onprogress = function updateProgress(event) {
-    if (event.lengthComputable) {
-        var percentComplete = Math.round((event.loaded / event.total) * 100);
-        console.log('Loading: ' + percentComplete + ' %');
-    }
-};
-request.onload = function(event) {
-
-    console.log('running');
-
-    var mjpegBuffer = request.response;
-
-    console.log(arrayBuffer2String(mjpegBuffer));
-    /*
-    if (mjpegBuffer) {
-        var byteArray = new Uint8Array(mjpegBuffer);
-        for (var i = 0; i < byteArray.byteLength; i++) {
-
-            var currentByte = mjpegBuffer[i];
-            var nextByte = mjpegBuffer[i + 1];
-            var thirdByte = mjpegBuffer[i + 2];
-            var fourthByte = mjpegBuffer[i + 3];
-
-
-            if (!started) {
-                if (currentByte == startNum && nextByte == nextNum) {
-                    console.log('started');
-                    started = true;
-                    imageBytes = [];
-                    imageBytes.push(currentByte);
+                    if (marker[0] == 255 && marker[1] == 216) {
+                        start = offset;
+                        break;
+                    }
                 }
             }
-            else {
-                if (currentByte == endPos.charCodeAt(0) && nextByte == endPos.charCodeAt(1) && thirdByte == endPos.charCodeAt(2) && fourthByte == endPos.charCodeAt(3)) {
-                    console.log('done');
-                    done = true;
-                    started = false;
 
-                    // got JPG data
-                    console.log(imageBytes);
-                }
-                else {
-                    imageBytes.push(currentByte);
+            // Look for the end of the JPEG
+            for (offset; offset < byteArray.byteLength - 1; offset++) {
+                marker[0] = byteArray[offset];
+                marker[1] = byteArray[offset + 1];
+
+                if (marker[0] == 255 && marker[1] == 217) {
+                    end = offset;
+
+                    // Grab image if an end is found
+                    image = byteArray.subarray(start, end);
+
+                    // Display
+                    display(image);
+
+                    // Remove image from incoming buffer
+                    byteArray = byteArray.subarray(end);
+
+                    // Reset values
+                    start = 0;
+                    offset = 0;
+
+                    // Denote that an image has been found
+                    value = true;
                 }
             }
+
+            // No image found by default
+            return value;
         }
+
+        //arrayBuffer2String(mjpegBuffer);
+    };
+
+    request.send(null);
+
+    // Grab the existing image
+    var imgEl = document.getElementById('stream');
+
+    function display(byteArray) {
+
+        // Overwrite it with new data
+        var stringData = String.fromCharCode.apply(null, new Uint16Array(byteArray));
+        var encodedData = window.btoa(stringData);
+        var dataURI = "data:image/jpeg;base64," + encodedData;
+        
+        console.log(dataURI);
+        
+        imgEl.src = dataURI;
     }
-    */
-};
 
-request.send(null);
-
-function arrayBuffer2String(buf) {
-    var result = '';
-    if (buf) {
-        var bufView = new Uint16Array(buf);
-        for (var i = 0, len = bufView.byteLength; i < len; i++) {
-            console.log('Converting: ' + Math.round((i / len) * 100) + ' %');
-            result = result + String.fromCharCode(bufView[i]);
-        }
+    function arrayBuffer2String(buf) {
+        var bb = new Blob([buf]);
+        var f = new FileReader();
+        f.onload = function(e) {
+            console.log(e.target.result);
+        };
+        f.readAsText(bb);
     }
-    return result;
-}
-
-function string2ArrayBuffer(str) {
-    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-    var bufView = new Uint16Array(buf);
-    for (var i = 0, strLen = str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-}
-
+}, false);
